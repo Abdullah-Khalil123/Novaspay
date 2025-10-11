b1bf28b22e5833002825f99e3e571461e4fc3afc
Abdullah <abdullah.khalil123321@gmail.com>
2025-10-11 14:48:26 +0500

    'index'

--- novaspay-frontend/src/pages/Index/index.tsx
+++ novaspay-frontend/src/pages/Index/index.tsx
@@ -1,122 +1,190 @@
-import { useTransactions } from '@/hooks/useTransaction';
-import type { Transaction } from '@/types/transaction';
-import { Info, ArrowDown, ArrowRight } from 'lucide-react';
 import Draggable from '@/components/custom/dragable';
+import { useAccounts } from '@/hooks/useAccounts';
+import { useKYCs } from '@/hooks/useKYC';
+import { useTransactions } from '@/hooks/useTransaction';
+import type { Account } from '@/types/accounts';
+import type { KYC } from '@/types/kyc';
+// import router from '@/router';
+import type { Transaction } from '@/types/transaction';
+import type { TFunction } from 'i18next';
+import { ArrowDown, ArrowRight, Copy } from 'lucide-react';
 import { useState } from 'react';
-import { QRCodeSVG } from 'qrcode.react';
-import { createInviteLink } from '@/actions/client';
-import { toast } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+const maskAccountNumber = (accountNumber: string) => {
+  if (!accountNumber) return '';
+  const visiblePart = accountNumber.slice(-5); // last 5 characters
+  const maskedPart = '*'.repeat(Math.max(0, accountNumber.length - 5));
+  return maskedPart + visiblePart;
+};
 
 const IndexPage = () => {
   const { t } = useTranslation();
-  const [inviteCode, setInviteCode] = useState<string | null>(null);
-  const [show, setShow] = useState<boolean | null>(null);
+  const router = useNavigate();
+  const [showDetails, setShowDetails] = useState<Account | null>();
 
   const { data } = useTransactions({
     page: 1,
     limit: 10,
   });
+  const { data: accountData } = useAccounts();
+  const account: Account = accountData?.data[0];
 
   const transactions: Transaction[] = data?.data || [];
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(inviteCode || '');
-    toast.success(t('Invitation code copied to clipboard!'));
-  };
-
-  const handleCreateInvite = async () => {
-    try {
-      const data = await createInviteLink();
-      const code = data.data.code;
-      setInviteCode(code);
-      setShow(true);
-    } catch (err) {
-      alert(t('Failed to generate invite.'));
-    }
-  };
-
-  const inviteLink = `${
-    import.meta.env.VITE_CLIENT_URL
-  }/company/create-account-login?invitationCode=${inviteCode}`;
-
   return (
     <div className="px-padding pt-2">
       <div>
-        <div className="flex justify-end w-full">
-          <p
-            onClick={handleCreateInvite}
-            className="text-red-400 text-sm underline cursor-pointer"
+        {/* KYC AUDIT */}
+        {/* {
+          <div
+            className={
+              'p-5 mt-4 flex items-center justify-between rounded-lg ' +
+              (Kyc?.status === 'FAILED' ? ' bg-[#fce2e2]' : ' bg-[#d0d0d4]')
+            }
           >
-            {t('Invite to Register')}
-          </p>
-        </div>
-
-        <div className="p-5 mt-4 flex items-center justify-between bg-[#d6d6da] rounded-lg">
-          <div className="flex gap-5 text-[#6b6868]">
-            <Info />
-            <p>{t('To switch wallet accounts, please click the button!')}</p>
+            <div
+              className={
+                'gap-5 ' +
+                (Kyc?.status === 'FAILED' ? 'text-[#f36b6e]' : 'text-[#6b6868]')
+              }
+            >
+              <h3 className="text-xl font-bold">KYC Audit {Kyc?.status}</h3>
+              <p>{Kyc?.reason}</p>
+            </div>
+            <button
+              onClick={async () => {
+                getAllKYCs()
+                  .then(() => {
+                    navigate('/member/client/documentForm?mode=upd');
+                  })
+                  .catch(() => {
+                    navigate('/member/client/documentForm?mode=crt');
+                  });
+              }}
+              className="bg-sidebar-bg text-sm cursor-pointer text-button-text px-4 py-2 rounded-sm"
+            >
+              Update Account
+            </button>
           </div>
-          <a
-            href={import.meta.env.VITE_CLIENT_URL + '/user/login'}
-            className="bg-sidebar-bg cursor-pointer text-button-text px-4 py-2 rounded-md"
-          >
-            {t('Switch wallet user function')}
-          </a>
+        } */}
+
+        <div className="bg-white p-6 w-full max-w-[500px] mt-8 rounded-lg text-[#6B7280]">
+          <div className="flex items-center justify-between">
+            <div className="bg-[#dbeafd] text-[#2463eb] w-fit p-1 px-2 text-2xl rounded-md">
+              $
+            </div>
+            <p>{maskAccountNumber(account?.accountNumber as string)}</p>
+            <p>USD {t('Account')}</p>
+          </div>
+          <div className="flex">
+            <h1 className="text-3xl font-bold text-text-primary my-4">
+              ${account?.balance}
+            </h1>
+            <button
+              onClick={() => {
+                setShowDetails(account);
+              }}
+              className='className="text-center w-full text-red-400'
+            >
+              {t('Views')}
+            </button>
+          </div>
         </div>
 
-        <div className="bg-secondary mt-8 p-3.5 rounded-md border border-border">
+        <div className="bg-secondary mt-8 p-3.5 rounded-md border-border-color border-[1px]">
           <h2 className="text-3xl font-bold mb-4">
             {t('Recent Transactions')}
           </h2>
-          <Table data={transactions} />
+          <Table data={transactions} t={t} />
         </div>
       </div>
-
-      {show && (
+      {showDetails && (
         <Draggable
-          className="bg-background px-4"
-          title={t('Invite User to Register')}
-          Open={setShow}
-          children={
-            <div className="p-6 rounded-xl shadow-lg flex flex-col items-center gap-4 w-[280px] text-center">
-              <QRCodeSVG value={inviteLink} size={230} />
+          className="px-8 min-w-[700px] text-sm space-y-3 py-6 bg-background shadow-lg rounded-md"
+          title="Account Details"
+          Open={setShowDetails}
+        >
+          <p>
+            Account Balance:{' '}
+            <span className="font-bold">{showDetails.balance}</span>
+          </p>
+          <button
+            onClick={() => {
+              router('/banking/history/history?accountId=' + showDetails.id);
+            }}
+            className="
+              px-4 py-1 bg-sidebar-child/90 text-white rounded-sm cursor-pointer hover:bg-sidebar-child/45 focus:outline-none focus:ring-2 focus:ring-sidebar-bg focus:ring-offset-2
+            "
+          >
+            {t('History')}
+          </button>
+          <p>
+            Bank Name:{' '}
+            <span className="font-bold">{showDetails.bankingName}</span>
+          </p>
+          <p>
+            Banking Address:{' '}
+            <span className="font-bold">{showDetails.bankingAddress}</span>
+          </p>
+          <p>
+            Baneficiary Name:{' '}
+            <span className="font-bold">{showDetails.accountName}</span>
+          </p>
+          <p>
+            IBAN Number:{' '}
+            <span className="font-bold">{showDetails.ibanNumber}</span>
+          </p>
+          <p>
+            Account Number:{' '}
+            <span className="font-bold">{showDetails.accountNumber}</span>
+          </p>
+          <p>
+            Currency: <span className="font-bold">{showDetails.currency}</span>
+          </p>
 
-              <code className="bg-[#f3f4f6] p-2 rounded-md w-full break-all">
-                <p className="text-black text-sm break-all">{inviteCode}</p>
-              </code>
+          <div>
+            <button
+              className="flex items-center gap-2 border border-border rounded-sm px-2 py-1 mx-auto hover:bg-sidebar-child/10 cursor-pointer"
+              onClick={() => {
+                const formattedDetails = `
+Account Balance: ${showDetails.balance || ''}
+Bank Name: ${showDetails.bankingName || ''}
+Banking Address: ${showDetails.bankingAddress || ''}
+Beneficiary Name: ${showDetails.accountName || ''}
+IBAN Number: ${showDetails.ibanNumber || ''}
+Account Number: ${showDetails.accountNumber || ''}
+Currency: ${showDetails.currency || ''}
+          `.trim();
 
-              <div className="flex gap-2 items-center text-text-primary">
-                <button
-                  onClick={handleCopy}
-                  className="cursor-pointer border border-border px-4 text-nowrap py-2 rounded-sm"
-                >
-                  {t('Copy Code')}
-                </button>
-                <a
-                  href={inviteLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-nowrap border border-border px-4 py-2 rounded-sm"
-                >
-                  {t('Go to Page')}
-                </a>
-              </div>
-            </div>
-          }
-        />
+                navigator.clipboard.writeText(formattedDetails);
+                toast.success('Account details copied to clipboard!', {
+                  position: 'top-center',
+                });
+              }}
+            >
+              <Copy size={16} />
+              Copy
+            </button>
+          </div>
+        </Draggable>
       )}
     </div>
   );
 };
 
-const Table = ({ data }: { data: Transaction[] }) => {
-  const { t } = useTranslation();
-
+const Table = ({
+  data,
+  t,
+}: {
+  data: Transaction[];
+  t: TFunction<'translation', undefined>;
+}) => {
   return (
     <table className="w-full">
       <thead>
-        <tr className="text-text-primary border-b border-border">
+        <tr className="text-[#A3a6ad] border-b-[1px] border-border-color">
           <th className="py-2">{t('Description')}</th>
           <th>{t('Date')}</th>
           <th>{t('Amount')}</th>
@@ -129,9 +197,8 @@ const Table = ({ data }: { data: Transaction[] }) => {
           <tr
             key={index}
             className={
-              index % 2 === 0
-                ? 'border-b border-border'
-                : 'border-b border-border bg-background'
+              (index % 2 === 0 ? '' : ' bg-background') +
+              ' border-b-[1px] border-border-color'
             }
           >
             <td className="h-10">
@@ -140,14 +207,17 @@ const Table = ({ data }: { data: Transaction[] }) => {
               ) : (
                 <ArrowDown className="inline mr-1" color="green" />
               )}{' '}
-              {t(item.orderType as string)}
+              {item.orderType}
             </td>
             <td>{item.updatedAt}</td>
             <td
               className={`${
-                item.orderType === 'TRANSFER' ? 'text-green-600' : ''
+                item.orderType !== 'TRANSFER'
+                  ? 'text-[#ed4248]'
+                  : 'text-green-600'
               }`}
             >
+              {item.orderType === 'TRANSFER' ? '+' : '-'}
               {item.amount}
               <span className="text-text-primary"> (USD)</span>
             </td>
@@ -155,7 +225,7 @@ const Table = ({ data }: { data: Transaction[] }) => {
             <td>
               <div className="text-sm text-white flex justify-center">
                 <p
-                  className={`w-fit min-w-26 px-2 rounded-sm ${
+                  className={`w-fit px-2 min-w-26 rounded-sm ${
                     item.status === 'SUCCESS'
                       ? 'bg-[#69c242] '
                       : item.status === 'PENDING'
@@ -165,7 +235,7 @@ const Table = ({ data }: { data: Transaction[] }) => {
                       : 'bg-orange-700/80'
                   }`}
                 >
-                  {t(item.status as string)}
+                  {item.status}
                 </p>
               </div>
             </td>
